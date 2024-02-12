c26ab398bf9321e6e7b47f6484afbcda9d5d94b9
shivam8962 <shivamsingh8962@gmail.com>
2024-02-13 00:59:45 +0530

    Lecture 13-14

--- Lecture 13-14/variables_scope.js
+++ Lecture 13-14/variables_scope.js
@@ -1,11 +1,10 @@
-/*
-var:Not a Blocked scope,function scope,global scope, redeclaration and updatation is possible
-let:Blocked scope,not a functional scope, can't be redeclaration , but variable updation is possible
-const:Blocked scope,not a functional scope, can't be redeclaration and updated
-*/
+// /*
+// var:Not a Blocked scope,function scope,global scope, redeclaration and updatation is possible
+// let:Blocked scope,not a functional scope, can't be redeclaration , but variable updation is possible
+// const:Blocked scope,not a functional scope, can't be redeclaration and updated
+// */
 
-
-// 1. simple variable declaration:
+// // 1. simple variable declaration:
 
 // let a =  10;
 // const b = 20;
@@ -15,9 +14,7 @@ const:Blocked scope,not a functional scope, can't be redeclaration and updated
 // console.log(b);
 // console.log(c);
 
-
-
-// 2. Blocked scope: checking let var const one by one
+// // 2. Blocked scope: checking let var const one by one
 
 
 // {
@@ -27,31 +24,31 @@ const:Blocked scope,not a functional scope, can't be redeclaration and updated
 // }
 // console.log(c);
 
-// 3.functional scope
+// // 3.functional scope
 
-// function scope(){
-//     if(true) {
-//         const a = 20;
-//         console.log(a)
+// // function scope(){
+// //     if(true) {
+// //         const a = 20;
+// //         console.log(a)
   
-//     }
-//     console.log(a)
+// //     }
+// //     console.log(a)
     
-// }
-// scope()
+// // }
+// // scope()
 
-// 4. global scope and functionl scope:
+// // 4. global scope and functionl scope:
 
-var a = 10;
-function scope(){
-    var b = 20;
-    if(true) {
-        var c = 30;
-        console.log(a)
-        console.log(b)
-        console.log(c)
-    }
-}
-scope()
-console.log(c)
+// // var a = 10;
+// // function scope(){
+// //     var b = 20;
+// //     if(true) {
+// //         var c = 30;
+// //         console.log(a)
+// //         console.log(b)
+// //         console.log(c)
+// //     }
+// // }
+// // scope()
+// // console.log(c)
 
