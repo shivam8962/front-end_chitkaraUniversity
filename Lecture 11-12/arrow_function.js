c2972d9e713fcc000d314b0622bcb2e8886c3d96
shivam8962 <shivamsingh8962@gmail.com>
2024-02-13 00:47:42 +0530

    Lecture 11-12

--- Lecture 11-12/arrow_function.js
+++ Lecture 11-12/arrow_function.js
@@ -12,3 +12,10 @@ const squares = (num) =>{
     return num*num;
 }
 console.log(squares(3))
+
+// 3. Arrow function in array:
+
+// let arr = [2,45,767,35,12]
+// arr.forEach((val)=>{
+//     console.log(val);
+// })
