2908a2e4fe8daeee8979518aa66f14e1ed142ce5
shivam8962 <shivamsingh8962@gmail.com>
2024-02-13 00:48:10 +0530

    Lecture 11-12

--- Lecture 11-12/callback_function.js
+++ Lecture 11-12/callback_function.js
@@ -1,8 +1,17 @@
-const cal = (a,b,operation) => {
-    return operation(a,b);
-}
+//1. Call Back:
+// let arr = [1,23,4,46,37]
 
- const result = cals(3,4, function(num1,num2){
-    return num1 + num2;
+// arr.forEach(myfunc);  // function calls
+// function myfunc(val,index,arr){
+//     console.log(val)
+// }
+
+//2.
+const cal = (a, b, operation) => {
+  return operation(a, b);
+};
+
+const result = cals(3, 4, function (num1, num2) {
+  return num1 + num2;
 });
 console.log(result);
