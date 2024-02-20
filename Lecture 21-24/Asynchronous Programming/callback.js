85a396f62ffb213e3b63a56641ef16a15ef8b714
shivam8962 <shivamsingh8962@gmail.com>
2024-02-20 07:04:51 +0530

    Lecture 21-24

--- Lecture 21-24/Asynchronous Programming/callback.js
+++ Lecture 21-24/Asynchronous Programming/callback.js
@@ -1,21 +1,17 @@
-/*
 // Making 2 normal function to see function
 
 //1.
-function one() {
-  console.log("This is a 1st regular function");
-}
+// function one() {
+//   console.log("This is a 1st regular function");
+// }
 
-function two() {
-  console.log("This is a 2nd regular function");
-}
+// function two() {
+//   console.log("This is a 2nd regular function");
+// }
+// // calling both function one by one
+// one();
+// two();
 
-// calling both function one by one
-one();
-two();
-*/
-
-/*
 //2. making connection of func 2 inside func 1: we do that using argument
 function one(call_two) {
   console.log("This is a 1st function, please call function 2");
@@ -25,10 +21,7 @@ function one(call_two) {
 function two() {
   console.log("This is a 2nd regular function");
 }
-
-// calling both function one by one
 one(two);
-*/
 
 //write a program for starting a production ones the order is received.
 
@@ -42,23 +35,23 @@ one(two);
 // };
 // order(production);
 
-let stocks = {
-  fruits: ["grapes", "bananas", "apple", "strawberry"],
-  liquid: ["water", "ice"],
-  holder: ["cone", "cup", "stick"],
-  toppings: ["chocolate", "peanuts"],
-};
+// let stocks = {
+//   fruits: ["grapes", "bananas", "apple", "strawberry"],
+//   liquid: ["water", "ice"],
+//   holder: ["cone", "cup", "stick"],
+//   toppings: ["chocolate", "peanuts"],
+// };
 
-let order = (fruit_name, call_production) => {
-  setTimeout(() => {
-    console.log(`${stocks.fruits[fruit_name]} was selected`);
-  }, 2000);
-  call_production();
-};
+// let order = (fruit_name, call_production) => {
+//   setTimeout(() => {
+//     console.log(`${stocks.fruits[fruit_name]} was selected`);
+//   }, 2000);
+//   call_production();
+// };
 
-let production = () => {
-  setTimeout(() => {
-    console.log("Production started");
-  }, 1000);
-};
-order(0, production);
+// let production = () => {
+//   setTimeout(() => {
+//     console.log("Production started");
+//   }, 1000);
+// };
+// order(0, production);
