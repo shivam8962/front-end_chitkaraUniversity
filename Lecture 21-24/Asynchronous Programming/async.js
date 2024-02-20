19c1987be7a276bb5fff8ce5b06e6e0154e0ea6d
shivam8962 <shivamsingh8962@gmail.com>
2024-02-20 06:51:37 +0530

    lecture 21-24

--- Lecture 21-24/Asynchronous Programming/async.js
+++ Lecture 21-24/Asynchronous Programming/async.js
@@ -1,11 +1,11 @@
 // synchronous programming: JS runs in synchronous manner. Top to botton orderin serial order.
-console.log("I");
-console.log("Eat");
-console.log("Chocolate");
-console.log("with");
-console.log("Rich");
+// console.log("I");
+// console.log("Eat");
+// console.log("Chocolate");
+// console.log("with");
+// console.log("Rich");
 
-// Asynchronous programming: T0 see the asynchronous behaviour we were using a setTimeOut function which is inbuild funtion that allow us to run our function after a specific amount of time.
+// Asynchronous programming: To see the asynchronous behaviour we were using a setTimeOut function which is inbuild funtion that allow us to run our function after a specific amount of time.
 console.log("I");
 console.log("Eat");
 setTimeout(() => {
