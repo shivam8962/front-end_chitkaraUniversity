cf8b275f4bbf01b98b79d4ab0dd2ba39bf0d7da7
shivam8962 <shivamsingh8962@gmail.com>
2024-02-20 06:38:42 +0530

    Lecture 21-24

--- Lecture 21-24/setTimeOut.js
+++ Lecture 21-24/setTimeOut.js
@@ -1,21 +1,22 @@
 // setimeout func is used to execute a func after a specified delay
 
+// Basic structure: setTimeout(function,delay)
+
 // 1.
 // console.log("Hi");
 
-// setTimeout(function(){
-//     console.log("There!!");
-// },2000);
+// setTimeout(function () {
+//   console.log("There!!");
+// }, 2000);
 
-// console.log("This is a working of SetTimeout function")
+// console.log("SetTimeout function is working");
 
-// 2.
+// 2. clearTimeout: Its a function that cancel the execution of the function specified in setTimeout.
 
 console.log("Hi");
 
-let a = setTimeout(function(){
-    console.log("There!!");
-},2000);
-clearTimeout(a)
-console.log("This is a working of SetTimeout function")
-
+let a = setTimeout(function () {
+  console.log("There!!");
+}, 2000);
+clearTimeout(a);
+console.log("SetTimeout function is not working");
