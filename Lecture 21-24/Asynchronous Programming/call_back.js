85bf2cf202b19ce54993553f7d0314cca99e6440
shivam8962 <shivamsingh8962@gmail.com>
2024-02-24 00:00:10 +0530

    Lecture 21-24

---
+++ Lecture 21-24/Asynchronous Programming/call_back.js
@@ -0,0 +1,28 @@
+//1.simple function:
+
+function first(){
+    console.log("This is a first function")
+}
+
+function second(){
+    console.log("This is a second function")
+}
+
+//calling:
+first()
+second()
+
+
+
+
+
+//2. callback
+function first(){
+    console.log("This is a first function")
+}
+
+function second(call){
+    console.log("This is a second function")
+    call()
+}
+second(first) // callback
