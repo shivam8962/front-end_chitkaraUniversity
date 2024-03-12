855b21b8a2eb5a95c9da18f7ee0539b22c4988ae
shivam8962 <shivamsingh8962@gmail.com>
2024-03-13 02:12:28 +0530

    Lecture 32

---
+++ Lecture 32/Revision/api.js
@@ -0,0 +1,20 @@
+//1.
+// let url="https://jsonplaceholder.typicode.com/todos/";
+// fetch(url).then((data)=>{
+//     console.log(data);
+//     return data.json();
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+//2.
+// aysnc /await
+// let url="https://jsonplaceholder.typicode.com/todos/";
+// async function getData(){
+//     let data=await fetch(url);
+//    let resultdata= await data.json();
+//    console.log(resultdata);
+// }
+// getData();
